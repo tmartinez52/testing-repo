a93662a5036b4cbbed724acd64525e49f2713bc8
Tim Martinez <34745639+tmartinez52@users.noreply.github.com>
2024-02-07 00:38:19 +0000

    adding env to code

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ parser.add_argument('--source-url', type=str, help='source url')
 parser.add_argument('--source-token', type=str, help='source token')
 parser.add_argument('--destination-url', type=str, help='dest url')
 parser.add_argument('--destination-token', type=str, help='dest token')
+parser.add_argument('--environment', type=str, help='The deployment environment (e.g., development, staging, production)')
+
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -16,6 +18,20 @@ source_url = args.source_url if args.source_url is not None else os.environ.get(
 source_token = args.source_token if args.source_token is not None else os.environ.get('SOURCE_TOKEN')
 destination_url = args.destination_url if args.destination_url is not None else os.environ.get('DESTINATION_URL')
 destination_token = args.destination_token if args.destination_token is not None else os.environ.get('DESTINATION_TOKEN')
+destination_token = args.destination_token if args.destination_token is not None else os.environ.get('DESTINATION_TOKEN')
+environment = arg.environment if args.environment is not None else os.environ.get('ENVIRONMENT')
+
+if args.environment:
+    if args.environment == 'dev':
+        print("Environment: dev")
+    elif args.environment == 'sandbox':
+        print("Environment: sandbox")
+    elif args.environment == 'main':
+        print("Environment: production")
+    else:
+        print("Environment not recognized.")
+else:
+    print("No environment specified.")
 
 # Use the determined values in your code
 print(f"Using source One: {source_url}")
